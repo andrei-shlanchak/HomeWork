0a94d3f8be60fcf82b2f6d7ac2e8b032f3a81044
SvA-Stone <83873142+SvA-Stone@users.noreply.github.com>
2021-05-23 13:16:07 -0700

    Add files via upload

---
+++ HomeWork_1.py
@@ -0,0 +1,5 @@
+name_f = input('Whats your name?')
+name_l = input('Whats your lastname?')
+age = (int(input('How old are you?')))
+name = input('Whats your question?')
+print(name_f, name_l, age, name)
